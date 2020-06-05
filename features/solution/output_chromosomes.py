ab9760e5101116e1c039f64d0d01566201b599b8
Christland <acquayechristabel@gmail.com>
2020-06-05 14:30:48 +0000

    exce fix

---
+++ features/solution/output_chromosomes.py
@@ -0,0 +1,5 @@
+from features.solution.chromosome_def import generate_population
+
+
+if __name__ == "__main__":
+    pass
--- test.py
+++ test.py
@@ -1,6 +1,19 @@
-from openpyxl import Workbook
+from openpyxl import Workbook, load_workbook
 
-for i in range(0, 4):
+
+def create_sheet(size, headings):
     book = Workbook()
     sheet = book.active
-    book.create_sheet("Chromosome ", i)
+    for i in range(size):
+        book.create_sheet("Chromosome ", (i))
+    book.save('test.xlsx')
+
+def insert_into_excel(row, column, data):
+    book = Workbook()
+    sheet = book.active
+    sheet.cell(row=row, column=column).value = data
+    book.save('test.xlsx')
+
+if __name__ == "__main__":
+   row, column, data = 5, 5, 'CE 587'
+   insert_into_excel(row = row, column = column, data = data)
